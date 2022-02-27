119a0073e77bbc7523833f6936819660f03658e2
Harry Stern <harry@harrystern.net>
2022-02-27 05:18:42 -0500

    Try spawning subprocess inside test

--- examples/test.rs
+++ examples/test.rs
@@ -1,7 +1,18 @@
 use tarpaulin_missing_coverage::*;
 
 fn main() {
-    do_the_thing();
+    let args: Vec<_> = std::env::args().into_iter().collect();
+    if args.contains(&"do_the_thing".into()) {
+        do_the_thing();
+        return;
+    }
+
+    let exe_path = std::env::current_exe().unwrap();
+
+    std::process::Command::new(exe_path)
+        .args(&["do_the_thing"])
+        .spawn()
+        .unwrap();
 }
 
 #[test]
