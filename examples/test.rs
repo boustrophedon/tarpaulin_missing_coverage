5c69c23e2660e1144722583833bd66456ef31629
Harry Stern <harry@harrystern.net>
2022-02-27 06:37:21 -0500

    Add test name to parameters for subprocess
    Also change function to write to a file and check that it was actually
    written to in the CI

--- examples/test.rs
+++ examples/test.rs
@@ -2,6 +2,7 @@ use tarpaulin_missing_coverage::*;
 
 fn main() {
     let args: Vec<_> = std::env::args().into_iter().collect();
+    println!("args: {:?}", args);
     if args.contains(&"do_the_thing".into()) {
         do_the_thing();
         return;
@@ -10,7 +11,7 @@ fn main() {
     let exe_path = std::env::current_exe().unwrap();
 
     std::process::Command::new(exe_path)
-        .args(&["do_the_thing"])
+        .args(&["call_main", "--", "do_the_thing"])
         .spawn()
         .unwrap();
 }
